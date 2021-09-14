0adf649079bbb683a389db58dd9a47a47a6ea426
chalilayang <chalilayang@sohu-inc.com>
2021-09-14 22:39:31 +0800

    doGet

--- src/com/servlet/MyServlet.java
+++ src/com/servlet/MyServlet.java
@@ -7,29 +7,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 @WebServlet(name = "MyServlet")
 public class MyServlet extends HttpServlet {
-
-    @Override
-    protected void doPost(
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) throws ServletException, IOException {
-        response.setContentType("text/html");
-        // 实际的逻辑是在这里
-        PrintWriter out = response.getWriter();
-        out.println("<h1>" +  "Hello World" + "</h1>");
-    }
 
     @Override
     protected void doGet(
             HttpServletRequest request,
             HttpServletResponse response
     ) throws ServletException, IOException {
-        response.setContentType("text/html");
-        // 实际的逻辑是在这里
+        response.setContentType("text/html;charset=UTF-8");
+
         PrintWriter out = response.getWriter();
-        out.println("<h1>" +  "Hello World" + "</h1>");
+        String title = "HTTP Header 请求实例 - 菜鸟教程实例";
+        String docType =
+                "<!DOCTYPE html> \n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><meta charset=\"utf-8\"><title>" + title + "</title></head>\n"+
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<table width=\"100%\" border=\"1\" align=\"center\">\n" +
+                "<tr bgcolor=\"#949494\">\n" +
+                "<th>Header 名称</th><th>Header 值</th>\n"+
+                "</tr>\n");
+
+        Enumeration headerNames = request.getHeaderNames();
+
+        while(headerNames.hasMoreElements()) {
+            String paramName = (String)headerNames.nextElement();
+            out.print("<tr><td>" + paramName + "</td>\n");
+            String paramValue = request.getHeader(paramName);
+            out.println("<td> " + paramValue + "</td></tr>\n");
+        }
+        out.println("</table>\n</body></html>");
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -5,12 +5,18 @@
   Time: 16:05
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>坤坤</title>
-  </head>
-  <body>
-  Hello
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>菜鸟教程(runoob.com)</title>
+</head>
+<body>
+<form action="MyServlet" method="GET">
+  网址名：<input type="text" name="name">
+  <br />
+  网址：<input type="text" name="url" />
+  <input type="submit" value="提交" />
+</form>
+</body>
 </html>
